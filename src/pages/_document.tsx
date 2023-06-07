5be144044dc3febed2382e5adb05ad673d1149b1
RobotMasterxo <peperaru@gmail.com>
2023-06-08 00:25:33 +0200

    Adapting to gh-pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-[url('/background.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+      <body className="bg-no-repeat bg-cover bg-center bg-fixed" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/background.jpg)` }}>
         <Main />
         <NextScript />
       </body>
